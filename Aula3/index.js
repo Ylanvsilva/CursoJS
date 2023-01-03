060efc9870c820207ae00d59d539a3de0b297013
Ylan <77461194+Ylanvsilva@users.noreply.github.com>
2023-01-02 23:33:56 -0300

    Aula3 + Ex1

---
+++ Aula3/index.js
@@ -0,0 +1,9 @@
+//* Strings = Texto
+
+console.log("'Ylan Vinicio'");
+console.log('"Ylan Vinicio"');
+console.log(`'Ylan' "Vinicio"`);
+
+//* Number = Número
+
+console.log(12, 15.85, 'Ylan Vinicio ')
