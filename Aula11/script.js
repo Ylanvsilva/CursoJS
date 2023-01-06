8f3be8d83eaf53c53fc9fe2756c36d25880dd5bd
Ylan <77461194+Ylanvsilva@users.noreply.github.com>
2023-01-05 21:03:52 -0300

    Aula11 + Ex1

---
+++ Aula11/script.js
@@ -0,0 +1,16 @@
+window.alert('Com a nossa mensagem.');
+
+window.confirm('Deseja realmente apagar?');
+
+window.prompt('Digite seu nome:');
+
+const confirma = confirm('Realmente deseja apagar?'); //* Faz com que a variável <confirma> passe a ter um valor
+
+let confirma1 = confirm('Qualquer mensagem');
+
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+
+num1 = parseFloat(num1); //* Fazendo a conversão de string para number
+
+num2 = Number(num2);
