30a2435549ed2fd4d9a6fa249ee9dc50664a0e4f
Ylan <77461194+Ylanvsilva@users.noreply.github.com>
2023-02-04 18:08:07 -0300

    Aula17

---
+++ Aula17/script.js
@@ -0,0 +1,27 @@
+function saudacao (nome) {
+    return `Bom dia! ${nome}`;
+}
+
+const variavel = saudacao('Ylan');
+console.log(variavel);
+
+//?-----
+
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(2, 2));
+
+//?-----
+
+//* Arrow function
+    const Raiz = n => n ** 0.5;
+//*
+
+const raiz = function (n) {
+    return n ** 0.5
+};
+
+console.log(raiz(9))
