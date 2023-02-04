059b5ad0d34e52581472d5280a6a94ad1d65bc3a
Ylan <77461194+Ylanvsilva@users.noreply.github.com>
2023-02-04 19:56:24 -0300

    Aula19

---
+++ Aula19/script.js
@@ -0,0 +1,7 @@
+//? Valor primitivo e por referencia
+
+/**
+ * * Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol) - valor
+ * 
+ * ! Referência (mutável) - array, object, function - Passados por referência
+ */
