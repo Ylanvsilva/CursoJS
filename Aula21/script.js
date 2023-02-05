9f2914f210f323226858b8f797e7fcec5333d0bd
Ylan <77461194+Ylanvsilva@users.noreply.github.com>
2023-02-04 21:25:52 -0300

    Aula21

---
+++ Aula21/script.js
@@ -0,0 +1,14 @@
+//? Operadores de comparação
+
+/**
+ * * > Maior que
+ * * >= Maior que ou igual a
+ * * < Menor que
+ * * <= Menor que ou igual a
+ * * == igualdade
+ * * = Atribuição
+ * * == Igualdade (valor) !!!
+ * * === Igualdade estrita (valor e tipo)
+ * * != Diferente (valor) !!!
+ * * !== Diferente estrito (valor e tipo)
+ */
