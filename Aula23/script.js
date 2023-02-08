ca06ee678ad8098401b289fa157fff5bface89f5
Ylan <77461194+Ylanvsilva@users.noreply.github.com>
2023-02-07 22:59:34 -0300

    Aula22

---
+++ Aula23/script.js
@@ -0,0 +1,15 @@
+//* Avaliação de verdadeiro e falso
+
+/**
+ * ! && -> false && true -> false "o valor mesmo"
+ * ? || -> true || false -> vai retornar "o valor verdeiro"
+ * 
+ * * Avaliados em falso
+ * * FALSY
+ * * false
+ * * 0
+ * * '' "" ``
+ * * null / undefined
+ * * NaN
+ */   
+
