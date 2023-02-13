63a1e51d876e6eb9d785bb7a3186c7e6d4c8a8c9
Ylan <77461194+Ylanvsilva@users.noreply.github.com>
2023-02-13 18:12:49 -0300

    Aula27

---
+++ Aula27/script.js
@@ -0,0 +1,19 @@
+//* Operação ternaria
+
+//! (condição) ? 'Valor para verdadeiro' : 'Valor para falso'; 
+
+const pontuacaoUsuario = 999;
+
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+
+const corUsuario = 'Pink';
+
+const corPadrao = corUsuario || 'Preta';
+
+console.log(nivelUsuario, corPadrao);
+
+// if (pontuacaoUsuario >= 1000) {
+//     console.log('Usuario VIP');
+// } else {
+//     console.log('Usuario normal');
+// };
