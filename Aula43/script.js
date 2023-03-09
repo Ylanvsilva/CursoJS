5fe4e936bcff287e0518c62d7f6ada2a64272fa4
Ylan <77461194+Ylanvsilva@users.noreply.github.com>
2023-03-09 20:34:30 -0300

    Aula43

--- Aula43/script.js
+++ Aula43/script.js
@@ -6,6 +6,20 @@
  ** Numero e divisivel por 5 = Buzz
  ** Numero e divisivel por 3 e 5 = FizzBuzz
  ** Numero NÃO e divisivel por 3 e 5 = retorna o proprio numero
- ** Checar se o numero e realmente um numero
+ ** Checar se o numero e realmente um numero = retorna o proprio numero
  ** Use a função com numeros de 0 a 100
  */
+
+function fizzbuzz(numero) {
+    if (typeof numero !== 'number') return numero;
+    if (numero % 3 && numero % 5 === 0) return 'FizzBuzz';
+    if (numero % 3 === 0) return 'Fizz';
+    if (numero % 5 === 0) return 'Buzz';
+    return numero;
+}
+
+console.log('a', fizzbuzz('a'));
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i, fizzbuzz(i))
+};
